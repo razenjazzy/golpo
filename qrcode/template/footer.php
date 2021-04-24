d1cccbb66da8e795fea2b3104ef1984230af53e5
razen <razen@golpocom.com>
2021-04-23 19:53:41 -0500

    initial commit

---
+++ qrcode/template/footer.php
@@ -0,0 +1,21 @@
+<div class="pt-3">
+    <div class="container">
+        <hr>
+        <div class="row pt-5 pb-4">
+            <div class="col-6">
+                <iframe width="360" height="240" src="https://www.youtube.com/embed/HPgRcmtiARU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div class="col-6">
+                <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fgolpocom&tabs=messages&width=360&height=240&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=842219099513398" width="360" height="240" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+            </div>
+        </div>
+        <hr>
+        <div class="row py-2 small mb-3">
+            <div class="col-6">
+                <?php echo getString('title') . ' &copy; ' . 'Golpo Communications Limited | ' .date('Y') ; ?>
+            </div>
+            <div class="col-6">
+            </div>
+        </div>
+    </div>
+</div>
---
+++ qrcode/template/header.php
@@ -0,0 +1,12 @@
+<div class="bg-primary position-relative">
+    <div class="overlay-gradient"></div>
+    <div class="container position-relative">
+        <div class="row py-5">
+            <div class="col">
+                <h1 class="display-1">QR Code Generator</h1>
+                <p>This project is working under <br>Golpo Communications Limited</p>
+                <p><a href="https://golpocom.com" class="btn btn-primary btn-lg shadow" role="button">Learn more &raquo;</a></p>
+            </div>
+        </div>
+    </div>
+</div>
